06509ebb473ce65f4f62fc84b94fa9c0cc69ee18
sanghmitra2023 <128592793+sanghmitra2023@users.noreply.github.com>
2023-05-31 10:11:44 +0000

    add js code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//window.onload = function() {};
+
+const totalCards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const randomNumber = totalCards[Math.floor(Math.random() * totalCards.length)];
+const randomNumberElement = document.getElementById("randomNumber");
+randomNumberElement.innerHTML = randomNumber;
+
+const suits = [
+  { suit: "♦", color: "red" },
+  { suit: "♥", color: "red" },
+  { suit: "♠", color: "black" },
+  { suit: "♣", color: "black" }
+];
+const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+const randomSuitElement = document.querySelectorAll(".randomSuit");
+
+randomSuitElement.forEach(element => {
+  element.innerHTML = randomSuit.suit;
+  element.style.color = randomSuit.color;
+  randomNumberElement.style.color = randomSuit.color;
+});
+
+/*document.getElementById("button").addEventListener("click", function() {
+  window.location.reload();
+});
+*/
+function reloadThePage() {
+  window.location.reload();
+}
+setInterval(reloadThePage, 10000);
