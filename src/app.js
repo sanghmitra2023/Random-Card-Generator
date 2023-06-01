a08ce8f6ee1a1f120bc0ed520b74b1d474956373
sanghmitra2023 <128592793+sanghmitra2023@users.noreply.github.com>
2023-06-01 11:09:19 +0000

    make all changes

--- src/app.js
+++ src/app.js
@@ -5,32 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-//window.onload = function() {};
-
 const totalCards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-const randomNumber = totalCards[Math.floor(Math.random() * totalCards.length)];
+const suits = ["♦", "♥", "♠", "♣"];
+
+function getRandomElementFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+const randomSuits = getRandomElementFromArray(suits);
+const randomNumber = getRandomElementFromArray(totalCards);
+
 const randomNumberElement = document.getElementById("randomNumber");
 randomNumberElement.innerHTML = randomNumber;
 
-const suits = [
-  { suit: "♦", color: "red" },
-  { suit: "♥", color: "red" },
-  { suit: "♠", color: "black" },
-  { suit: "♣", color: "black" }
-];
-const randomSuit = suits[Math.floor(Math.random() * suits.length)];
-const randomSuitElement = document.querySelectorAll(".randomSuit");
+const randomSuitElement = document.querySelector(".randomSuit");
+randomSuitElement.innerHTML = randomSuits;
 
-randomSuitElement.forEach(element => {
-  element.innerHTML = randomSuit.suit;
-  element.style.color = randomSuit.color;
-  randomNumberElement.style.color = randomSuit.color;
-});
+const randomSuitElementTwo = document.querySelector(".randomSuitTwo");
+randomSuitElementTwo.innerHTML = randomSuits;
 
-/*document.getElementById("button").addEventListener("click", function() {
+if (randomSuits === "♦" || randomSuits === "♥") {
+  randomSuitElement.style.color = "red";
+  randomNumberElement.style.color = "red";
+  randomSuitElementTwo.style.color = "red";
+}
+
+document.getElementById("button").addEventListener("click", function() {
   window.location.reload();
 });
-*/
+
 function reloadThePage() {
   window.location.reload();
 }
