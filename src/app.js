55bb277a30f04dfbdeada386be590e63fbfd2ca9
sanghmitra2023 <128592793+sanghmitra2023@users.noreply.github.com>
2023-06-03 12:34:17 +0000

    another way to do it

--- src/app.js
+++ src/app.js
@@ -54,3 +54,33 @@ function applyChanges() {
     card.style.height = `${cardHeight}px`;
   }
 }
+
+/*const totalCards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const randomNumber = totalCards[Math.floor(Math.random() * totalCards.length)];
+const randomNumberElement = document.getElementById("randomNumber");
+randomNumberElement.innerHTML = randomNumber;
+
+const suits = [
+  { suit: "♦", color: "red" },
+  { suit: "♥", color: "red" },
+  { suit: "♠", color: "black" },
+  { suit: "♣", color: "black" }
+];
+const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+const randomSuitElement = document.querySelectorAll(".randomSuit");
+
+randomSuitElement.forEach(element => {
+  element.innerHTML = randomSuit.suit;
+  element.style.color = randomSuit.color;
+  randomNumberElement.style.color = randomSuit.color;
+});
+
+document.getElementById("button").addEventListener("click", function() {
+  window.location.reload();
+});
+
+function reloadThePage() {
+  window.location.reload();
+}
+setInterval(reloadThePage, 10000);
+*/
