5ff052693ca93ac5094e2ccc525c2768a8d3aad0
sanghmitra2023 <128592793+sanghmitra2023@users.noreply.github.com>
2023-06-27 09:18:19 +0000

    final

--- src/app.js
+++ src/app.js
@@ -2,9 +2,6 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 const totalCards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 const suits = ["♦", "♥", "♠", "♣"];
 
@@ -37,50 +34,3 @@ function reloadThePage() {
   window.location.reload();
 }
 setInterval(reloadThePage, 10000);
-
-const cardWidthInput = document.getElementById("cardWidth");
-const cardHeightInput = document.getElementById("cardHeight");
-const applyButton = document.getElementById("applyButton");
-
-applyButton.addEventListener("click", applyChanges);
-
-function applyChanges() {
-  const cardWidth = cardWidthInput.value;
-  const cardHeight = cardHeightInput.value;
-  const card = document.querySelector(".card");
-
-  if (cardWidth && cardHeight) {
-    card.style.maxWidth = `${cardWidth}px`;
-    card.style.height = `${cardHeight}px`;
-  }
-}
-
-/*const totalCards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-const randomNumber = totalCards[Math.floor(Math.random() * totalCards.length)];
-const randomNumberElement = document.getElementById("randomNumber");
-randomNumberElement.innerHTML = randomNumber;
-
-const suits = [
-  { suit: "♦", color: "red" },
-  { suit: "♥", color: "red" },
-  { suit: "♠", color: "black" },
-  { suit: "♣", color: "black" }
-];
-const randomSuit = suits[Math.floor(Math.random() * suits.length)];
-const randomSuitElement = document.querySelectorAll(".randomSuit");
-
-randomSuitElement.forEach(element => {
-  element.innerHTML = randomSuit.suit;
-  element.style.color = randomSuit.color;
-  randomNumberElement.style.color = randomSuit.color;
-});
-
-document.getElementById("button").addEventListener("click", function() {
-  window.location.reload();
-});
-
-function reloadThePage() {
-  window.location.reload();
-}
-setInterval(reloadThePage, 10000);
-*/
